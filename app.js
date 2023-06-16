04bc219f17703012dbbf9c7547451611b80a0354
Artistry449 <artistry449@gmail.com>
2023-06-17 00:25:55 +0800

    Shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -2,27 +2,30 @@
 // Тоглогч 1 -> 0, Тоглогч  2 ->1
 var activePlayer = 1;
 // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
-var scores = [0,0];
+var scores = [0, 0];
 
 // Тоглогчийн өөрийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
 
 // Шооны аль талаараа буусныг хадгалах хувьсагч(1-6 дотор утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө)
-var dice = Math.floor(Math.random()*6)+1;
-
-// <div class="player-score" id="score-0">42</div>
-
-// document.querySelector('#score-1').innerHTML = '<em>Yes!</em>';
 
 // Програм эхлэхэд бэлтгэх
 
-document.querySelector('.dice').style = "display:none";
+document.getElementById('score-0').textContent = "0";
+document.getElementById('score-1').textContent = "0";
 
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
+document.getElementById('current-0').textContent = "0";
+document.getElementById('current-1').textContent = "0";
 
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
+var diceDom = document.querySelector('.dice');
+diceDom.style = "display:none";
 
+document.querySelector('.btn-roll').addEventListener('click',
+    function () {
+        var diceNumber = Math.floor(Math.random() * 6) + 1;
+        document.getElementById('current-0').textContent = diceNumber;
 
-console.log('Шоо: ' + dice);
+        diceDom.style = "display:block";
+        diceDom.src = "dice-" + diceNumber + ".png";
+
+    });
